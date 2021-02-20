ee5746e670887bab62aa11c0a5bdd72b7aea71f0
marcin <test@gmail.com>
2021-02-20 14:23:47 +0100

    Added todo-list-header component and move header section from app.component.html to todo-list-header.component.html. Added simply scss style for new component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
+import { TodoListHeaderComponent } from './todo-list-header/todo-list-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListHeaderComponent
   ],
     imports: [
         BrowserModule,
---
+++ src/app/todo-list-header/todo-list-header.component.ts
@@ -0,0 +1,26 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Todo} from "../todo";
+
+@Component({
+  selector: 'app-todo-list-header',
+  templateUrl: './todo-list-header.component.html',
+  styleUrls: ['./todo-list-header.component.scss']
+})
+export class TodoListHeaderComponent implements OnInit {
+
+  newTodo: Todo = new Todo();
+
+  @Output()
+  add: EventEmitter<Todo> = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  addTodo() {
+    this.add.emit(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+}
